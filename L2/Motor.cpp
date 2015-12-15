1857f215400427481688a2299f27f0de93ef5a3f
Lars Eckervogt <lars@formigas.de>
2015-12-15 10:36:20 +0100

    refactoring

--- L2/Motor.cpp
+++ L2/Motor.cpp
@@ -51,191 +51,161 @@
  * Additional linker flags: none
  */
 
-void handle(int sig) {
-	mraa::Pwm* pwm_pina = new mraa::Pwm(0);
-	mraa::Pwm* pwm_pinb = new mraa::Pwm(14);
-	std::cout << "Signal detected: " << sig << std::endl;
-	pwm_pina->config_percent(1, 0);
-	pwm_pinb->config_percent(1, 0);
+static mraa::Pwm* pwm_steering = new mraa::Pwm(0);
+static mraa::Pwm* pwm_engine = new mraa::Pwm(14);
+static mraa::Gpio* pin_standby = new mraa::Gpio(15);
+static mraa::Gpio* pin_forward = new mraa::Gpio(45);
+static mraa::Gpio* pin_backward = new mraa::Gpio(46);
+static mraa::Gpio* pin_left = new mraa::Gpio(47);
+static mraa::Gpio* pin_right = new mraa::Gpio(48);
 
+void handle(int sig) {
+	pwm_engine->config_percent(1, 0);
+	pwm_steering->config_percent(1, 0);
 	exit(0);
 }
 
+void reset_Engine() {
+    pin_forward->write(0);
+    pin_backward->write(0);
+    pwm_engine->config_percent(1, 0);
+}
+
+void reset_Steering() {
+    pin_left->write(0);
+    pin_right->write(0);
+    pwm_steering->config_percent(1, 0);
+}
+
 void stop() {
-	mraa::Pwm* pwm_pina = new mraa::Pwm(0);
-	mraa::Gpio* ina1_pin = new mraa::Gpio(45);
-	mraa::Gpio* ina2_pin = new mraa::Gpio(46);
+	pin_forward->write(1);
+	pin_backward->write(1);
+	pwm_engine->config_percent(0.5f, 1);
 
-	ina1_pin->write(1);
-	ina2_pin->write(1);
-	pwm_pina->config_percent(1, 1);
+    sleep(2);
+	//usleep(1500000);
 
-	usleep(1500000);
-
-	ina1_pin->write(0);
-	ina2_pin->write(0);
-	pwm_pina->config_percent(1, 0);
+    reset_Engine();
 }
 
 void onem_straight(bool forward) {
-	mraa::Pwm* pwm_pina = new mraa::Pwm(0);
-	mraa::Gpio* ina1_pin = new mraa::Gpio(45);
-	mraa::Gpio* ina2_pin = new mraa::Gpio(46);
-
-	ina1_pin->write(forward);
-	ina2_pin->write(!forward);
-	pwm_pina->config_percent(1, 0.2);
+	pin_forward->write(forward);
+	pin_backward->write(!forward);
+	pwm_engine->config_percent(1, 0.2);
 
 	usleep(3600000);
 	stop();
 }
 
-
-
-
 void left(int usec) {
+    pin_left->write(1);
+    pin_right->write(0);
+    pwm_steering->config_percent(1, 1);
+    usleep(usec);
+
+    reset_Steering();
 }
 
 void right(int usec) {
+    pin_left->write(0);
+    pin_right->write(1);
+    pwm_steering->config_percent(1, 1);
+    usleep(usec);
+
+    reset_Steering();
+}
+
+int init() {
+    if (pwm_engine == NULL) {
+        std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+    if (pwm_engine->enable(true) != MRAA_SUCCESS) {
+        std::cerr << "Cannot enable PWM on mraa::PWM object, exiting"
+        << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+    if (pwm_steering == NULL) {
+        std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+    if (pwm_steering->enable(true) != MRAA_SUCCESS) {
+        std::cerr << "Cannot enable PWM on mraa::PWM object, exiting"
+        << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+    if (pin_standby == NULL) {
+        std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+    if (pin_standby->dir(mraa::DIR_OUT) != MRAA_SUCCESS) {
+        std::cerr << "Can't set digital pin as output, exiting" << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+    if (pin_forward == NULL) {
+        std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+    if (pin_forward->dir(mraa::DIR_OUT) != MRAA_SUCCESS) {
+        std::cerr << "Can't set digital pin as output, exiting" << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+    if (pin_backward == NULL) {
+        std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+    if (pin_backward->dir(mraa::DIR_OUT) != MRAA_SUCCESS) {
+        std::cerr << "Can't set digital pin as output, exiting" << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+    if (pin_left == NULL) {
+        std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+    if (pin_left->dir(mraa::DIR_OUT) != MRAA_SUCCESS) {
+        std::cerr << "Can't set digital pin as output, exiting" << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+    if (pin_right == NULL) {
+        std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+    if (pin_right->dir(mraa::DIR_OUT) != MRAA_SUCCESS) {
+        std::cerr << "Can't set digital pin as output, exiting" << std::endl;
+        return MRAA_ERROR_UNSPECIFIED;
+    }
+
+    pin_standby->write(1);	// standby=1 means motor on
+    reset_Engine();
+    reset_Steering();
+
+    return mraa::SUCCESS;
 }
 
 int main() {
+    int ret = 0;
+
 	signal(SIGINT,handle);
 
-	// define pwm pins; 
-	// motor a is for going forward and backward
-	//motor b is for steering the wheels
-	mraa::Pwm* pwm_pina = new mraa::Pwm(0);
-	mraa::Pwm* pwm_pinb = new mraa::Pwm(14);
+    if((ret = init()) != 0)
+        return ret;
 
-	// check and enable pins
-	if (pwm_pina == NULL) {
-		std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-	// enable PWM on the selected pin
-	if (pwm_pina->enable(true) != MRAA_SUCCESS) {
-		std::cerr << "Cannot enable PWM on mraa::PWM object, exiting"
-				<< std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-	if (pwm_pinb == NULL) {
-		std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-	// enable PWM on the selected pin
-	if (pwm_pinb->enable(true) != MRAA_SUCCESS) {
-		std::cerr << "Cannot enable PWM on mraa::PWM object, exiting"
-				<< std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-
-	// define in and  and standy pins
-	mraa::Gpio* stby_pin = new mraa::Gpio(15);
-	mraa::Gpio* ina1_pin = new mraa::Gpio(45);
-	mraa::Gpio* ina2_pin = new mraa::Gpio(46);
-	mraa::Gpio* inb1_pin = new mraa::Gpio(47);
-	mraa::Gpio* inb2_pin = new mraa::Gpio(48);
-
-	// check an enable the pins
-	if (stby_pin == NULL) {
-		std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-	// set the pin as output
-	if (stby_pin->dir(mraa::DIR_OUT) != MRAA_SUCCESS) {
-		std::cerr << "Can't set digital pin as output, exiting" << std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-
-	if (ina1_pin == NULL) {
-		std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-	// set the pin as output
-	if (ina1_pin->dir(mraa::DIR_OUT) != MRAA_SUCCESS) {
-		std::cerr << "Can't set digital pin as output, exiting" << std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-
-	if (ina2_pin == NULL) {
-		std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-	// set the pin as output
-	if (ina2_pin->dir(mraa::DIR_OUT) != MRAA_SUCCESS) {
-		std::cerr << "Can't set digital pin as output, exiting" << std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-
-	if (inb1_pin == NULL) {
-		std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-	// set the pin as output
-	if (inb1_pin->dir(mraa::DIR_OUT) != MRAA_SUCCESS) {
-		std::cerr << "Can't set digital pin as output, exiting" << std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-	if (inb2_pin == NULL) {
-		std::cerr << "Can't create mraa::Pwm object, exiting" << std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-	// set the pin as output
-	if (inb2_pin->dir(mraa::DIR_OUT) != MRAA_SUCCESS) {
-		std::cerr << "Can't set digital pin as output, exiting" << std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-
-	// set the motor action control pins
-	stby_pin->write(0);	// standby=0 means motor off
-	ina1_pin->write(0); // a to 0/1 means go forward
-	ina2_pin->write(1);
-	inb1_pin->write(1); // b to 1/0 means go left/right (try out)
-	inb2_pin->write(0);
-
-	// PWM duty_cycle value, 0.0 == 0%, 1.0 == 100%
-	float duty_cycle = 0.0;
-
-	// select a pulse width period of 1ms
-	int period = 1;
-
-	float dir = 0.0;
-
-	// turn on motor driver
-	stby_pin->write(1);
-
+    sleep(2);
+    std::cout << "Breaking now" << std::endl;
 	stop();
-	onem_straight(true);
-	onem_straight(true);
-	onem_straight(true);
+    std::cout << "Done breaking" << std::endl;
+	// onem_straight(true);
+	// onem_straight(true);
+	// onem_straight(true);
 
-	sleep(2);
-	onem_straight(false);
-	onem_straight(false);
-	onem_straight(false);
+	// sleep(2);
+	// onem_straight(false);
+	// onem_straight(false);
+	// onem_straight(false);
 
+	// // Reset
+    reset_Engine();
+    reset_Steering();
 
-	// this makes the car go forward or backward with low speed
-	//std::cout << "Testdrive" << std::endl; 
-	//pwm_pina->config_percent(period, 0.3);
-	//pwm_pinb->config_percent(period*2, 0.1);
-
-	//// Drive straight
-	//sleep(6);
-
-	//// Drive left
-	//inb1_pin->write(0);
-	//inb2_pin->write(1);
-	//pwm_pinb->config_percent(period, 1);
-	//usleep(1500000); 
-
-	//// Drive straight
-	//pwm_pinb->config_percent(period, 0);
-	//sleep(4);
-
-	// Stop
-	pwm_pina->config_percent(period, 0);
-	pwm_pinb->config_percent(period, 0);
-	return mraa::SUCCESS;
+	return 0;
 }
---
+++ L2/copy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+scp Motor.cpp Makefile root@192.168.42.1:/home/root/
